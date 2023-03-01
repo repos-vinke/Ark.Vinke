e3bec97bcc65874cbc13dcf0ee2d9e3e0d7da0cd
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-03-01 15:30:56 -0300

    1. Rollback DynamicXml concept to modules instead of projects

--- 1.0.0/Modules/Ark.Vinke.Library/Sources/Ark.Vinke.Library/LibDynamicXml.cs
+++ 1.0.0/Modules/Ark.Vinke.Library/Sources/Ark.Vinke.Library/LibDynamicXml.cs
@@ -21,7 +21,7 @@ namespace Ark.Vinke.Library
     {
         #region Variables
 
-        private Dictionary<String, LibDynamicXmlElement> projectDictionary;
+        private Dictionary<String, LibDynamicXmlElement> moduleDictionary;
 
         #endregion Variables
 
@@ -29,7 +29,7 @@ namespace Ark.Vinke.Library
 
         public LibDynamicXml()
         {
-            this.projectDictionary = new Dictionary<String, LibDynamicXmlElement>();
+            this.moduleDictionary = new Dictionary<String, LibDynamicXmlElement>();
         }
 
         #endregion Constructors
@@ -43,18 +43,18 @@ namespace Ark.Vinke.Library
         /// <param name="xPathDynamicXml">The xPath of the dynamic xml element</param>
         public void LoadDynamicXml(LazyXml xml, String xPathDynamicXml)
         {
-            XmlNodeList xmlNodeProjectList = xml.ReadNodeChildList(xPathDynamicXml);
+            XmlNodeList xmlNodeModuleList = xml.ReadNodeChildList(xPathDynamicXml);
 
-            foreach (XmlNode xmlNodeProject in xmlNodeProjectList)
+            foreach (XmlNode xmlNodeModule in xmlNodeModuleList)
             {
-                LoadDynamicXmlNode(xml, xmlNodeProject, this[xmlNodeProject.Name]);
+                LoadDynamicXmlNode(xml, xmlNodeModule, this[xmlNodeModule.Name]);
 
-                XmlAttributeCollection xmlAttributeCollection = xml.ReadNodeAttributeList(xmlNodeProject);
+                XmlAttributeCollection xmlAttributeCollection = xml.ReadNodeAttributeList(xmlNodeModule);
                 foreach (XmlAttribute attribute in xmlAttributeCollection)
-                    this[xmlNodeProject.Name].Attribute[attribute.Name] = attribute.Value;
+                    this[xmlNodeModule.Name].Attribute[attribute.Name] = attribute.Value;
             }
 
-            xmlNodeProjectList = null;
+            xmlNodeModuleList = null;
         }
 
         /// <summary>
@@ -94,14 +94,14 @@ namespace Ark.Vinke.Library
         /// </summary>
         public void SaveDynamicXml(LazyXml xml, XmlNode xmlNodeDynamicXml)
         {
-            foreach (KeyValuePair<String, LibDynamicXmlElement> project in this.projectDictionary)
+            foreach (KeyValuePair<String, LibDynamicXmlElement> module in this.moduleDictionary)
             {
-                XmlNode xmlNodeProject = xml.WriteNode(xmlNodeDynamicXml, project.Key);
+                XmlNode xmlNodeModule = xml.WriteNode(xmlNodeDynamicXml, module.Key);
 
-                SaveDynamicXmlNode(xml, xmlNodeProject, project.Value);
+                SaveDynamicXmlNode(xml, xmlNodeModule, module.Value);
 
-                foreach (KeyValuePair<String, String> attribute in project.Value.Attribute.Collection)
-                    xml.WriteNodeAttribute(xmlNodeProject, attribute.Key, attribute.Value);
+                foreach (KeyValuePair<String, String> attribute in module.Value.Attribute.Collection)
+                    xml.WriteNodeAttribute(xmlNodeModule, attribute.Key, attribute.Value);
             }
         }
 
@@ -131,30 +131,30 @@ namespace Ark.Vinke.Library
 
         #region Properties
 
-        public Dictionary<String, LibDynamicXmlElement> Projects
+        public Dictionary<String, LibDynamicXmlElement> Modules
         {
-            get { return this.projectDictionary; }
+            get { return this.moduleDictionary; }
         }
 
         #endregion Properties
 
         #region Indexers
 
-        public LibDynamicXmlElement this[String project]
+        public LibDynamicXmlElement this[String module]
         {
             get
             {
-                if (this.projectDictionary.ContainsKey(project) == false)
-                    this.projectDictionary.Add(project, new LibDynamicXmlElement(project));
+                if (this.moduleDictionary.ContainsKey(module) == false)
+                    this.moduleDictionary.Add(module, new LibDynamicXmlElement(module));
 
-                return this.projectDictionary[project];
+                return this.moduleDictionary[module];
             }
             set
             {
-                if (this.projectDictionary.ContainsKey(project) == false)
-                    this.projectDictionary.Add(project, value);
+                if (this.moduleDictionary.ContainsKey(module) == false)
+                    this.moduleDictionary.Add(module, value);
                 else
-                    this.projectDictionary[project] = value;
+                    this.moduleDictionary[module] = value;
             }
         }
 
