7a16ca255ad242eb0728fb4e6789a21a12aef352
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-03-05 11:02:15 -0300

    1. Added Ark.Vinke.Studio

---
+++ 1.0.0/Modules/Ark.Vinke/Sources/Ark.Vinke.Studio/Components/Forms/FormMain.cs
@@ -0,0 +1,46 @@
+﻿// FormMain.cs
+//
+// This file is integrated part of Lazy project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 05
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Windows;
+using Lazy.Vinke.Windows.Forms;
+
+namespace Ark.Vinke.Studio
+{
+    public partial class FormMain : LazyForm
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion Constructors
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
---
+++ 1.0.0/Modules/Ark.Vinke/Sources/Ark.Vinke.Studio/Program.cs
@@ -0,0 +1,37 @@
+// Program.cs
+//
+// This file is integrated part of Lazy project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 05
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Windows;
+using Lazy.Vinke.Windows.Forms;
+
+namespace Ark.Vinke.Studio
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormMain());
+        }
+    }
+}
