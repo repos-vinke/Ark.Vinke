ab0d7dd80b79d29a8b7effb8ef408c6a22012b40
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-03-06 13:22:25 -0300

    1. Added LibStringFormat definition

---
+++ 1.0.0/Modules/Ark.Vinke.Library/Sources/Ark.Vinke.Library/LibStringFormat.cs
@@ -0,0 +1,27 @@
+﻿// LibStringFormat.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 06
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Globalization;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+
+namespace Ark.Vinke.Library
+{
+    public static class LibStringFormat
+    {
+        public static class DateTime
+        {
+            public static string ISO8601Z = "yyyy-MM-ddTHH:mm:ss:fffZ";
+        }
+    }
+}
