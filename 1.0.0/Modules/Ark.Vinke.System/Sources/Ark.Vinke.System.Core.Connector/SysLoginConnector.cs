ce82d246700d1c47445c305fa355817bebac2677
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-03-12 20:15:52 -0300

    1. Added new Project (Connector) to support client calls to the servers

---
+++ 1.0.0/Modules/Ark.Vinke.System/Sources/Ark.Vinke.System.Core.Connector/SysLoginConnector.cs
@@ -0,0 +1,73 @@
+﻿// SysLoginConnector.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 12
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Net.Http;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Json;
+
+using Ark.Vinke.Library;
+using Ark.Vinke.Library.Core;
+using Ark.Vinke.Library.Core.Connector;
+
+using Ark.Vinke.Framework;
+using Ark.Vinke.Framework.Core;
+using Ark.Vinke.Framework.Core.Data;
+using Ark.Vinke.Framework.Core.IServer;
+using Ark.Vinke.Framework.Core.Connector;
+
+using Ark.Vinke.Facilities;
+using Ark.Vinke.Facilities.Core;
+using Ark.Vinke.Facilities.Core.Data;
+using Ark.Vinke.Facilities.Core.IServer;
+using Ark.Vinke.Facilities.Core.Connector;
+
+using Ark.Vinke.System;
+using Ark.Vinke.System.Core;
+using Ark.Vinke.System.Core.Data;
+using Ark.Vinke.System.Core.IServer;
+
+namespace Ark.Vinke.System.Core.Connector
+{
+    public class SysLoginConnector : FwkConnector
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Constructors
+
+        public SysLoginConnector()
+        {
+            this.Route = "Ark.Vinke.System/Core.Server/SysLoginServer";
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        /// <summary>
+        /// Authenticate
+        /// </summary>
+        /// <param name="loginDataRequest">The login request data</param>
+        /// <returns>The login response data</returns>
+        public SysLoginDataResponse Authenticate(SysLoginDataRequest loginDataRequest)
+        {
+            return (SysLoginDataResponse)InvokeServer("Authenticate", loginDataRequest, HttpMethod.Post);
+        }
+
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+    }
+}
