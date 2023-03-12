c04dabf691646e37910b5f1d3e1832e0ffcf184b
Isaac Saraiva <isaacsaraiva.vinke@outlook.com>
2023-03-12 19:57:06 -0300

    1. Added new Project (Connector) to support client calls to the servers

---
+++ 1.0.0/Modules/Ark.Vinke.Library/Sources/Ark.Vinke.Library.Core.Client/LibSessionClient.cs
@@ -0,0 +1,59 @@
+﻿// LibSessionClient.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 12
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+using Lazy.Vinke.Windows;
+using Lazy.Vinke.Windows.Forms;
+
+using Ark.Vinke.Library;
+using Ark.Vinke.Library.Core;
+using Ark.Vinke.Library.Core.Connector;
+
+namespace Ark.Vinke.Library.Core.Connector
+{
+    public static class LibSessionClient
+    {
+        #region Variables
+        #endregion Variables
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+
+        public static String ServerUrl
+        {
+            get { return LibSessionConnector.ServerUrl; }
+            set { LibSessionConnector.ServerUrl = value; }
+        }
+
+        public static LibAuthenticationMode AuthenticationMode
+        {
+            get { return LibSessionConnector.AuthenticationMode; }
+            set { LibSessionConnector.AuthenticationMode = value; }
+        }
+
+        public static LibSessionConnectorParameter Parameters
+        {
+            get { return LibSessionConnector.Parameters; }
+        }
+
+        #endregion Properties
+    }
+}
---
+++ 1.0.0/Modules/Ark.Vinke.Library/Sources/Ark.Vinke.Library.Core.Connector/LibSessionConnector.cs
@@ -0,0 +1,98 @@
+﻿// LibSessionConnector.cs
+//
+// This file is integrated part of Ark project
+// Licensed under "Gnu General Public License Version 3"
+//
+// Created by Isaac Bezerra Saraiva
+// Created on 2023, March 12
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Collections.Generic;
+
+using Lazy.Vinke;
+
+using Ark.Vinke.Library;
+using Ark.Vinke.Library.Core;
+
+namespace Ark.Vinke.Library.Core.Connector
+{
+    public static class LibSessionConnector
+    {
+        #region Variables
+
+        private static LibSessionConnectorParameter parameter;
+
+        #endregion Variables
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+
+        public static String ServerUrl { get; set; }
+
+        public static LibAuthenticationMode AuthenticationMode { get; set; }
+
+        public static LibSessionConnectorParameter Parameters
+        {
+            get
+            {
+                if (parameter == null)
+                    parameter = new LibSessionConnectorParameter();
+
+                return parameter;
+            }
+        }
+
+        #endregion Properties
+    }
+
+    public class LibSessionConnectorParameter
+    {
+        #region Variables
+
+        private Dictionary<String, String> parameterDictionary;
+
+        #endregion Variables
+
+        #region Constructor
+
+        public LibSessionConnectorParameter()
+        {
+            this.parameterDictionary = new Dictionary<String, String>();
+        }
+
+        #endregion Constructor
+
+        #region Methods
+        #endregion Methods
+
+        #region Properties
+        #endregion Properties
+
+        #region Indexers
+
+        public String this[String name]
+        {
+            get
+            {
+                if (this.parameterDictionary.ContainsKey(name) == false)
+                    this.parameterDictionary.Add(name, null);
+
+                return this.parameterDictionary[name];
+            }
+            set
+            {
+                if (this.parameterDictionary.ContainsKey(name) == false)
+                    this.parameterDictionary.Add(name, value);
+                else
+                    this.parameterDictionary[name] = value;
+            }
+        }
+
+        #endregion Indexers
+    }
+}
